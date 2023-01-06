d9f0e634bde654af1f189c9ccdbd538a1d80eaba
phuonglinh99vn <linh.bbasep17s057@spjain.org>
2023-01-06 19:47:00 +1100

    First Commit

---
+++ dance_company/src/main/java/dance_company/usermanagement/web/LoginServlet.java
@@ -0,0 +1,43 @@
+package dance_company.usermanagement.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import connection.DbCon;
+import dance_company.usermanagement.dao.*;
+import dance_company.usermanagement.model.*;
+
+@WebServlet("/user-login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			String email = request.getParameter("login-email");
+			String password = request.getParameter("login-password");
+
+			UserDAO udao = new UserDAO(DbCon.getConnection());
+			user user = udao.userLogin(email, password);
+			if (user != null) {
+				request.getSession().setAttribute("auth", user);
+//				out.println("user logged in");
+				response.sendRedirect("index.jsp");
+			} else {
+				out.println("Your email or password is incorrect");
+			}
+
+		} catch (ClassNotFoundException|SQLException e) {
+			e.printStackTrace();
+		} 
+
+	}
+}
